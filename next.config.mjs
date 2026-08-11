40f88ae416a7b6fc745e92a5952d4e53c5ee1d22
Anusha <anushaa102002@gmail.com>
2026-08-11 13:53:15 +0530

    Changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   // turbopack: {
-//   //   root: new URL("..", import.meta.url).pathname,
-//   // },
-// };
-
-// export default nextConfig;
